1453bcad517962779331aae3c4578b8e5677e440
Your Name <you@example.com>
2023-03-16 18:31:13 -0700

    printfnl

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*
+ * main - starting pt of program
+ *
+ * Return - 0 to indicate a successful run
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
