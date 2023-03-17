111c0f8660a65f4b81278c276769a091bef64adb
Your Name <you@example.com>
2023-03-17 05:19:07 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		++c;
+	}
+	putchar(10);
+
+	return (0);
+}
