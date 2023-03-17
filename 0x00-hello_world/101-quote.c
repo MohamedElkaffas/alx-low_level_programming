d804b72334501e88220b151782a4f66e65682d2c
Your Name <you@example.com>
2023-03-16 19:39:52 -0700

    printwithoutprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+    return(1);
+}
