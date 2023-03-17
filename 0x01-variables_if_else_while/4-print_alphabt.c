88016fe3b6fe29b394223d0ed87e27468eb91082
Your Name <you@example.com>
2023-03-17 06:18:26 -0700

    lu_except_e_q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,24 +3,27 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print the letters of the alphabet
+ * Description: print the letters of the alphabet except e, q
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        char c = 'a';
+	char c = 'a';
 
-        while (c <= 'z')
-        {
-		if (c == 'e' || c == 'q') continue;
-                putchar(c);
-                ++c;
-        }
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'z')
+		{
+			putchar(c);
+		}
 
-        putchar(10);
+		putchar(c);
+		++c;
+	}
 
-        return (0);
+	putchar(10);
+
+	return (0);
 }
-
