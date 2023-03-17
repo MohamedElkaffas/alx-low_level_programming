e163757afa8381360fc052b2cf73981d82760f6e
Your Name <you@example.com>
2023-03-16 19:44:17 -0700

    printwithoutprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
     return(1);
 }
