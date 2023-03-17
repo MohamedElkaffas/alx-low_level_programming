5a478cb02fe6d3418f8f4f354c9eca2c8bc6110a
Your Name <you@example.com>
2023-03-16 18:47:07 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - print the size of various data types
  *
- * Description: prints the size of various types on the computer it is compiled and run upom
+ * Description: prints the size of various types on the computer it is
+ * compiled and run upom
  *
  * Return: Always 0 (Success)
  */
@@ -15,6 +16,7 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return 0;
+
+	return (0);
 }
 
