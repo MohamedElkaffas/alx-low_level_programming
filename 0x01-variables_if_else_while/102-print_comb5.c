2eb95c57fae40f06c8c3f45dacb0e0f6c050f9d9
Your Name <you@example.com>
2023-03-17 14:27:30 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * main - print pairs of double digit combos
+ *
+ * Description: print pairs of double digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j < 10)
+		{
+			l = j + 1;
+
+			k = i;
+
+			while (k < 10)
+			{
+				while (l < 10)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar(',');
+
+						putchar(' ');
+					}
+
+					++l;
+
+				}
+
+				l = 0;
+
+				++k;
+			}
+
+			++j;
+		}
+
+		++l;
+	}
+
+	putchar(10);
+	return (0);
+}
