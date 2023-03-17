54756da5ee25ca4a689dec85794a1517035a176c
Your Name <you@example.com>
2023-03-17 09:00:17 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		++i;
+	}
+
+	i = 0;
+
+	while (i < 6)
+	{
+		putchar(i + 'a');
+		++i;
+	}
+
+	putchar(10);
+
+	return (0);
+}
