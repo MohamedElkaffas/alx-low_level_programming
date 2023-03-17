b7fbfed5168bcd3f6fe5a82e2f82c6a44e684e6f
Your Name <you@example.com>
2023-03-17 08:16:22 -0700

    cast

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i+'0');
+		++i;
+	}
+
+	putchar(10);
+
+	return (0);
+}
