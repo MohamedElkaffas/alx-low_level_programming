72c6731d4598b5ffefa53c0aee1deeb7a4160e35
Your Name <you@example.com>
2023-03-22 20:27:02 -0700

    4fibafv

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
-
 /**
  * main - first 98 fibonacci numbers
  *
  * Return: Always 0.
  */
-
-
 int main(void)
 {
-	unsigned long a = 1, b = 2, cnt = 2;
-	unsigned long c;
+	long double a0 = 0, a1 = 1, b0 = 0, b1 = 2, c0, c1;
 
-	printf("%lu, %lu", a, b);
-
-	while (cnt < 98)
+	printf("%.0Lf, %.0Lf, ", a1, b1);
+	for (int i = 1; i <= 98; ++i)
 	{
-		c = (unsigned long)a + b;
-		printf(", %lu", c);
-		a = b;
-		b = c;
-		++cnt;
+		c0 = a0 + b0;
+		c1 = a1 + b1;
+		while (c1 >= 100000)
+		{
+			c1 -= 100000, ++c0;
+		}
+		if (c0 == 0)
+		{
+			printf("%.0Lf", c1);
+		}
+		else
+		{
+			printf("%.0Lf", c0);
+			for (long double j = 10; j <= 10000; j *= 10)
+			{
+				if (j > c1)
+				{
+					putchar('0');
+				}
+			}
+			printf("%.0Lf", c1);
+		}
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		a0 = b0, a1 = b1, b0 = c0, b1 = c1;
 	}
-
 	printf("\n");
 	return (0);
 }
