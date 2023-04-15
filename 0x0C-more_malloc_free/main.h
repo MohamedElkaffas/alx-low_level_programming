0f7d639b29fdd81231c479ca9dbeaccd181d02cc
Your Name <you@example.com>
2023-04-14 18:28:22 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,5 +17,8 @@ char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
 
 #endif
