623338526496ef733738d73833ceac222e439dab
Your Name <you@example.com>
2023-04-17 20:04:27 -0700

    fnlikemacro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
-#endif /* !FUNCTION_LIKE_MACRO_H */
+#ifndef ABS_H_
+#define ABS_H_
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif
