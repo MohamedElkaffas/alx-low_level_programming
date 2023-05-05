f1898faaa58bace325ff4dba31549dca38105572
Your Name <you@example.com>
2023-05-04 18:38:56 -0700

    c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "bits.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: Pointing to a string of 0 and 1 chars
+ *
+ * Return: Decimal result
+ **/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, multiplier;
+	unsigned int num;
+
+	if (!b)
+		return (0);
+
+	for (i = num = 0; b[i] != 0; ++i)
+		if (b[i] != '0' && b[i] != '1')
+			return (num);
+
+	for (i -= 1, multiplier = 0; i >= 0; --i, ++multiplier)
+	{
+		if (b[i] != '0')
+			num += 1 << multiplier;
+	}
+
+	return (num);
+}
