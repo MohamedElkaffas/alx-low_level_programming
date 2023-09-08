9eb17b237101abc63b3bfb5e73863a776fe58917
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-08 23:59:14 +0000

    hashTable_impl

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key in a hash table
+ * @key: the key to find the index of
+ * @size: size of the array of indices
+ * Return: the index at which the key/value pair
+ * should be stored in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (!size)
+		return (0);
+
+	hash = hash_djb2(key);
+	return (hash % size);
+}
