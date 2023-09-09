848db04e920ee99dc8890b3f7b460992d25b1b87
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-09 00:34:46 +0000

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
+
 	if (!ht)
 		return (NULL);
 
 	ht->size = size;
 	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+
 	if (ht->array == NULL)
 		return (NULL);
 
