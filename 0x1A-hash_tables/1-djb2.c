6ebce63a86aefaec151f764b2e324b0a79da8056
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-09 00:32:41 +0000

    betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,14 +5,17 @@
  * @str: string to hash
  * Return: hash index corresponding to the string
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
+
 	int c;
 
 	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	
+	}
 	return (hash);
 }
