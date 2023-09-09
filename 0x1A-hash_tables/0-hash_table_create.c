5b4d063157e5e7a46224ffdb0e858a3dd3b8ef32
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-09 00:35:49 +0000

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,6 +4,7 @@
   * @size: size of the array of the hash table
   * Return: Pointer to the newly created hash table or null ptr if something goes wrong
   */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
     	hash_table_t *ht;
@@ -17,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t*));
 
 	if (ht->array == NULL)
 		return (NULL);
